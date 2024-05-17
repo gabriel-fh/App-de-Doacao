99dccf6351adeb8e468084b1181c0fc4e5fe2fec
unknown <lisboasanchisguido@gmail.com>
2024-05-17 20:41:54 -0300

    First GdCommit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,13 @@
 import "../global.css";
+
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import { Slot } from "expo-router";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <GestureHandlerRootView>
+      <Slot />
+    </GestureHandlerRootView>
+  );
 }
