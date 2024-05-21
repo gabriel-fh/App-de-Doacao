3a88a8e75bad6084f0582356f0205ece8b5671c1
fabiojr0 <fabiojunior321@yahoo.com.br>
2024-05-21 19:27:54 -0300

    Icones da nav

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,17 +1,54 @@
-import "../../global.css";
-
 import { Tabs } from "expo-router";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
 
-export default function Layout() {
+export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: { height: 70, backgroundColor: "#fff" },
+        tabBarItemStyle: { paddingBottom: 10 },
+        headerStyle: { backgroundColor: "#fff" },
+        headerTitleAlign: "center",
+        tabBarInactiveTintColor: "#c0c0c0",
+        tabBarActiveTintColor: "#0D62AD",
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ title: "Início", headerShown: false }}
+        options={{
+          title: "Início",
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome6 name={"house"} size={25} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="Map/Map"
-        options={{ title: "Mapa", headerShown: false }}
+        options={{
+          title: "Mapa",
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome5 name={"map-marked-alt"} size={25} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Campaign/Campaign"
+        options={{
+          title: "Campanhas",
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome5 name={"scroll"} size={25} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile/Profile"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome5 name={"user-circle"} size={25} color={color} />
+          ),
+        }}
       />
     </Tabs>
   );
