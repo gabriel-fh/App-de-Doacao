9db82fb6ae01cd168177999de82424ec475e03e4
Gabriel Hermenegildo <gabriel.f.hermenegildo@gmail.com>
2024-05-21 20:35:51 -0300

    feat: pedindo autorização do usuário para acessar sua localização

--- src/app/(tabs)/Map/Map.tsx
+++ src/app/(tabs)/Map/Map.tsx
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import {
   requestForegroundPermissionsAsync,
@@ -9,29 +9,36 @@ import {
 
 const Map = () => {
   const mapRef = useRef<MapView>(null);
+  const [region, setRegion] = useState(null);
 
-  async function requestLocationPermissions() {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-
-      if (granted) {
-        const currentPosition = await getCurrentPositionAsync({
-          accuracy: LocationAccuracy.Balanced,
-        });
+  // obtendo localização do usuário
+  useEffect(() => {
+    (async () => {
+      const { status } = await requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Aceesso a localização negado!');
+        return;
       }
-    } catch (error) {
-      console.log("MapScreen error: ", error);
-    } 
-  }
+
+      let location = await getCurrentPositionAsync({ accuracy: LocationAccuracy.Balanced });
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      });
+    })();
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
       <MapView
+      ref={mapRef}
       initialRegion={{
         latitude: -22.896272986246537, // região default por enquanto
         longitude: -43.104700078765426,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
       }}
         style={{ width: "100%", height: "100%" }}
         provider={PROVIDER_GOOGLE}
