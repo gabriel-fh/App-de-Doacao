d042fcc7fd09293380ba459a06f450fe99578151
Gabriel Hermenegildo <gabriel.f.hermenegildo@gmail.com>
2024-05-23 19:55:48 -0300

    feat: página de instituição

--- src/app/(tabs)/Map/Map.tsx
+++ src/app/(tabs)/Map/Map.tsx
@@ -1,11 +1,12 @@
 import { View } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import {
   requestForegroundPermissionsAsync,
   LocationAccuracy,
   getCurrentPositionAsync,
 } from "expo-location";
+import { router } from "expo-router";
 
 const Map = () => {
   const mapRef = useRef<MapView>(null);
@@ -32,6 +33,7 @@ const Map = () => {
     })();
   }, []);
 
+
   return (
     <View style={{ flex: 1 }}>
       <MapView
@@ -42,7 +44,22 @@ const Map = () => {
         showsUserLocation={true}
         loadingIndicatorColor="#000"
         loadingBackgroundColor="#f1f1f1"
-      />
+      >
+        {region && (
+          <Marker
+            coordinate={{
+              latitude: region?.latitude + 0.001,
+              longitude: region?.longitude,
+            }}
+            image={{
+              uri: "https://cdn-icons-png.flaticon.com/512/3619/3619129.png",
+            }}
+            onPress={() => {
+              router.navigate('Institution/Institution') 
+            }}
+          />
+        )}
+      </MapView>
     </View>
   );
 };
