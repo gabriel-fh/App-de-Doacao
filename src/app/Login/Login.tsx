9ad8ab0fb1fc824af8b003f9955cef2b8a033dc3
gabriel-fh <gabriel.f.hermenegildo>
2024-06-05 14:00:24 -0300

    feat: Tela de login

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,12 +1,104 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import { theme } from "@/Theme/theme";
+import { KeyboardAvoidingView } from "react-native";
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={styles.container}
+    >
+      <View style={styles.loginContainer}>
+        <View style={styles.loginHeader}>
+          <View
+            style={{
+              height: 2,
+              backgroundColor: theme.primary,
+              flex: 1,
+            }}
+          />
+          <Text
+            style={{
+              fontFamily: "Montserrat_700Bold",
+              fontSize: 30,
+              color: theme.primary,
+            }}
+          >
+            Login
+          </Text>
+          <View
+            style={{
+              height: 2,
+              flex: 1,
+              backgroundColor: theme.primary,
+            }}
+          />
+        </View>
+        <View
+          style={{
+            gap: 30,
+          }}
+        >
+          <Input placeholder={"Digite seu e-mail"} title={"E-mail"} />
+          <Input placeholder={"Digite sua senha"} title={"Senha"} />
+          <Button text={"Entrar"} onPress={() => console.log}></Button>
+        </View>
+        <View style={{ marginTop: 15, alignItems: "center" }}>
+          <Text
+            style={{
+              fontFamily: "Montserrat_500Medium",
+              fontSize: 14,
+            }}
+          >
+            Ainda não possui uma conta?
+          </Text>
+          <TouchableOpacity>
+            <Text
+              style={{
+                color: theme.primary,
+                marginTop: 10,
+                fontFamily: "Montserrat_600SemiBold",
+                fontSize: 16
+              }}
+            >
+              Cadastre-se
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
-export default Login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loginContainer: {
+    width: "90%",
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    elevation: 5,
+  },
+  loginHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+    marginBottom: 40,
+  },
+});
+
+export default Login;
---
+++ src/components/Input.tsx
@@ -0,0 +1,54 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import TextInputMask from "react-native-mask-input";
+
+type InputProps = {
+  placeholder: string;
+  mask?: any;
+  required?: boolean;
+  email?: boolean;
+  password?: boolean;
+  title: string;
+};
+
+const Input = ({...props}: InputProps) => {
+  return (
+    <View>
+      <View style={styles.inputContainer}>
+        <Text style={[styles.subTitle, styles.absoluteText]}>{props.title}</Text>
+        <TextInputMask
+          style={styles.input}
+          placeholder={props.placeholder}
+          placeholderTextColor="#999"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    borderWidth: 2,
+    borderColor: "#0D62AD",
+    borderRadius: 10,
+    position: "relative",
+  },
+  subTitle: {
+    fontSize: 17,
+    fontFamily: "Montserrat_600SemiBold",
+  },
+  absoluteText: {
+    position: "absolute",
+    top: -14,
+    paddingHorizontal: 5,
+    left: 15,
+    fontSize: 17,
+    color: "#0D62AD",
+    backgroundColor: "#fff",
+  },
+  input: {
+    margin: 12,
+  },
+});
+
+export default Input;
