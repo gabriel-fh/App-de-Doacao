1a803169f5653fed6bcbce08fc8933a653246b59
gabriel-fh <gabriel.f.hermenegildo>
2024-09-10 19:27:16 -0300

    feat: consumindo api na tela de pesquisar campanhas

--- src/app/(tabs)/Campaign/Campaign.tsx
+++ src/app/(tabs)/Campaign/Campaign.tsx
@@ -15,58 +15,6 @@ import { theme } from "@/Theme/theme";
 import { Campaign as TypeCampaign, Item } from "@/@types/app";
 
 const Campaign = () => {
-  const DATA: TypeCampaign[] = [
-    {
-      id: 1,
-      name: "Campanha 1",
-      description: "Descrição da campanha 1",
-      avatar: "https://picsum.photos/150",
-      donated_items_quantity: 10,
-      donated_items_objective: 100,
-      date: "2021-09-01",
-      banner: "https://picsum.photos/500/210",
-    },
-    {
-      id: 2,
-      name: "Campanha 2",
-      description: "Descrição da campanha 2",
-      avatar: "https://picsum.photos/150",
-      donated_items_quantity: 20,
-      donated_items_objective: 100,
-      date: "2021-05-15",
-      banner: "https://picsum.photos/500/210",
-    },
-    {
-      id: 3,
-      name: "Campanha 3",
-      description: "Descrição da campanha 3",
-      avatar: "https://picsum.photos/150",
-      donated_items_quantity: 30,
-      donated_items_objective: 100,
-      date: "2021-11-30",
-      banner: "https://picsum.photos/500/210",
-    },
-    {
-      id: 4,
-      name: "Campanha 4",
-      description: "Descrição da campanha 4",
-      avatar: "https://picsum.photos/150",
-      donated_items_quantity: 40,
-      donated_items_objective: 100,
-      date: "2021-10-20",
-      banner: "https://picsum.photos/500/210",
-    },
-    {
-      id: 5,
-      name: "Campanha 5",
-      description: "Descrição da campanha 5",
-      avatar: "https://picsum.photos/150",
-      donated_items_quantity: 50,
-      donated_items_objective: 100,
-      date: "2021-08-05",
-      banner: "https://picsum.photos/500/210",
-    },
-  ];
 
   const scrollViewRef = useRef<ScrollView>(null);
   const [showButton, setShowButton] = useState(false);
@@ -137,7 +85,7 @@ const Campaign = () => {
               <ActivityIndicator size="large" color={theme.primary} />
             </View>
           ) : (
-            DATA?.map((item, index) => (
+            campaigns?.map((item, index) => (
               <CampaignCard key={index} campaign={item} />
             ))
           )}
