77108e13ce39ae99d17527c15b2765fb983a2373
gabriel-fh <gabriel.f.hermenegildo>
2024-09-28 07:26:07 -0300

    agora vai

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -11,46 +11,19 @@ import { useFetchNews } from "@/hooks/News/useFetchNews";
 import CampaignCarousel from "@/components/CampaignCarousel";
 import { theme } from "@/Theme/theme";
 import { Skeleton } from "moti/skeleton";
-import axios from "axios";
 
 const index = () => {
   const { data: news, isLoading: isLoadingNews } = useFetchNews();
-
-  const [test, setTest] = React.useState();
-  const [err, setErr] = React.useState();
-
-  const [test2, setTest2] = React.useState();
-  const [err2, setErr2] = React.useState();
-
-  React.useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then((response) => {
-      setTest2(response.data);
-    }).catch((error) => {
-      setErr2(error?.response?.data || error);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    axios.get("https://admin.doacao.tech/api/news").then((response) => {
-      setTest(response.data);
-    }).catch((error) => {
-      setErr(error?.response?.data || error);
-    });
-  }, []);
 
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={{
         marginHorizontal: 12,
-        paddingVertical: 20,
+        paddingVertical: 20
       }}
     >
       <CampaignCarousel />
-      <Text>{JSON.stringify(test)}</Text>
-      <Text>{JSON.stringify(test2)}</Text>
-      <Text>{JSON.stringify(err)}</Text>
-      <Text>{JSON.stringify(err2)}</Text>
       <View style={{ gap: 10, marginTop: 20 }}>
         {isLoadingNews ? (
           <View style={{marginTop: 20}}>
